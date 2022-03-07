d2904d31a660af05807b7ddbe6ec6c38995530c2
Juan Opazo <dopazo178@gmail.com>
2022-03-06 21:25:16 -0500

    start actions and reducers

---
+++ blog/src/actions/index.js
@@ -0,0 +1,10 @@
+import jsonPlaceholder from "../apis/jsonPlaceholder";
+
+export const fetchPosts = () => async dispatch => {
+    const res = await jsonPlaceholder.get('/posts');
+
+    dispatch({
+        type: 'FETCH_POSTS',
+        payload: res
+    });
+};
